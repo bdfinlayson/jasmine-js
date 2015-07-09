c2b8d861ef044859447a2bfdcbbd73111b274a27
Bryan Finlayson <bdfinlayson@gmail.com>
2015-07-09 00:03:08 -0500

    Add tests

--- spec/greetingSpec.js
+++ spec/greetingSpec.js
@@ -7,18 +7,24 @@ describe('Greeting', function() {
   });
 
   it('should say greeting', function() {
-    greeting.say('hello');
-    expect(greeting.friendlyGreeting).toEqual('hello');
+    greeting.say('Hello');
+    expect(greeting.friendlyGreeting).toEqual('Hello');
   });
 
   it('should accept a string', function() {
-    greeting.isValidInput('hello');
+    greeting.isValidInput('Hello');
     expect(greeting.response).toEqual(true);
   });
 
   it('should not accept invalid input', function() {
     greeting.isValidInput(1900);
     expect(greeting.response).toEqual(false);
+  });
+
+  it('should greet a person', function() {
+    greeting.isValidInput('Bryan Finlayson');
+    greeting.say('Hello');
+    expect(greeting.greetPerson('Bryan Finlayson')).toEqual('Hello Bryan Finlayson');
   });
 });
 
